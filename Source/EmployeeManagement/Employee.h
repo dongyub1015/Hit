f67a2f05636abe89b23f7278d52285c6d830cc8f
sheung.kim <gmail.com>
2022-04-01 10:15:56 +0900

    [Feature] EmploymentManagement test case & member function update
    Signed-off-by: sheung.kim <gmail.com>

--- Source/EmployeeManagement/Employee.h
+++ Source/EmployeeManagement/Employee.h
@@ -1,8 +1,19 @@
 #pragma once
+#include<iostream>
+using namespace std;
 
 class Employee {
+public:
+	string employeeNum_;
+	string name_;
+	string cl_;
+	string phoneNum_;
+	string birthDay_;
+	string certi_;
+	Employee(string employeeNum, string name, string cl, string phoneNum, string birthDay, string certi) :
+		employeeNum_(employeeNum), name_(name), cl_(cl), phoneNum_(phoneNum), birthDay_(birthDay), certi_(certi) {
 
+	}
 
 };
-
 
