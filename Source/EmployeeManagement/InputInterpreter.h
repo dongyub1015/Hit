bc45f8fbe1094c01a4adde09c1f0ce6203222149
smallrap <hongju.j@gmail.com>
2022-04-01 13:53:48 +0900

    [Refactoring] 파일 입력 부분에 대한 예외 처리 추가
    빈 파일에 대한 예외 처리 추가 (eof 삭제)
    파일 open 실패에 대한 예외 처리

--- Source/EmployeeManagement/InputInterpreter.h
+++ Source/EmployeeManagement/InputInterpreter.h
@@ -9,13 +9,20 @@ using namespace std;
 class InputInterpreter {
 public:
     InputInterpreter(string fileName) : inputFileName_(fileName) {}
+    
+    //Todo : main의 파라미터로 넘어온 파일이름에 대한 유효성 검사 필요
 
     vector<string> readFile() {
-        ifstream fin(inputFileName_);
         vector<string> inputStrAll;
+        ifstream fin(inputFileName_);
+
+        if (fin.fail()) {
+            cout << "Fail to open Input file" << endl;
+            return inputStrAll;
+        }
+
         string line;
-        while (!fin.eof()) {
-            getline(fin, line);
+        while (getline(fin, line)) {
             inputStrAll.push_back(line);
         }
         fin.close();
--- Source/UserInterfaceTest/test.cpp
+++ Source/UserInterfaceTest/test.cpp
@@ -4,11 +4,10 @@
 using namespace std;
 
 TEST(InputInterpreterTest, ReadFileTest) {
-
     InputInterpreter input1("input_20_20_empty.txt");
     vector<string> vectorStr;
-    // Todo : 빈파일에 대한 예외처리 필요 
     EXPECT_EQ(vectorStr, input1.readFile());
+
     InputInterpreter input2("input_20_20_temp.txt");
     vectorStr.push_back("ADD, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV");
     EXPECT_EQ(vectorStr, input2.readFile());
@@ -58,6 +57,5 @@ TEST(InputInterpreterTest, CheckInputContentsTest) {
     vectorStr.push_back("SCH, ,-f, ,name,LDEXRI");
     vectorStr.push_back("MOD, , , ,name,VCUHLE HMU,birthday,19910808");
     vectorStr.push_back("SCH, , , ,name,FB NTAWR");
-    //vectorStr.push_back("");
     EXPECT_EQ(vectorStr, input.readFile());
 }
