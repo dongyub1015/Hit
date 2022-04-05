af7a37b31e52b8fa4b095251d5c68b7a498146d2
Dongyeb Choi <dongyub1015@gmail.com>
2022-04-05 11:29:46 +0900

    [Feature] add/search/del 함수 기 구현된 함수 호출로 구현 push_back/search으로 vector 성능이 낮아지는 것을 방지

--- Source/EmployeeManagement/Command.h
+++ Source/EmployeeManagement/Command.h
@@ -18,38 +18,38 @@ protected:
 	ValidChecker* validChecker_;
 	string type_;
 
-	vector<string> employeeResultToString(vector<Employee> employResult);
+	vector<string> employeeResultToString(vector<Employee*>* employResult);
 public:
 	Command() { validChecker_ = new ValidChecker(); }
 	void setCommandString(const string& commandStr) { commandStr_ = commandStr; }
 	vector<string> makeResult(EmployeeManagement* EM, const string& command);
-	virtual vector<Employee> runCommand(EmployeeManagement* EM) { return vector<Employee>(); }
+	virtual vector<Employee*>* runCommand(EmployeeManagement* EM) { return nullptr; }
 };
 
 class AddCommand :public Command {
 public:
 	AddCommand() { type_ = "ADD";}
-	virtual vector<Employee> runCommand(EmployeeManagement* EM) override;
+	virtual vector<Employee*>* runCommand(EmployeeManagement* EM) override;
 };
 
 
 class DelCommand :public Command {
 public:
 	DelCommand() { type_ = "DEL"; }
-	virtual vector<Employee> runCommand(EmployeeManagement* EM) override;
+	virtual vector<Employee*>* runCommand(EmployeeManagement* EM) override;
 };
 
 
 class SchCommand :public Command {
 public:
 	SchCommand() { type_ = "SCH"; }
-	virtual vector<Employee> runCommand(EmployeeManagement* EM) override;
+	virtual vector<Employee*>* runCommand(EmployeeManagement* EM) override;
 };
 
 
 class ModCommand :public Command {
 public:
 	ModCommand() { type_ = "MOD"; }
-	virtual vector<Employee> runCommand(EmployeeManagement* EM) override;
+	virtual vector<Employee*>* runCommand(EmployeeManagement* EM) override;
 };
 
